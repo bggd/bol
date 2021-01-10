bea47a76eb598bcbb36c767b8bc35798227cc58c
bggd <gameovera@live.com>
2021-01-10 12:45:24 +0900

    Add Area2D

---
+++ src/scene/area2d.lua
@@ -0,0 +1,12 @@
+local Area2D = {}
+
+local Node = require("scene.node")
+local Signal = require("scene.signal")
+
+function Area2D.create()
+  local obj = Node.create()
+  obj.areaEntered = Signal.create()
+  return obj
+end
+
+return Area2D
